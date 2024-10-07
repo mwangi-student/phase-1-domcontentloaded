40ecd38102649aca165575db7f49dfd656d71f02
chieby-mwangi <chiebyrah@gmail.com>
2024-10-07 23:57:01 +0300

    phase-1-domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.getElementById("text");
+  textElement.textContent = "This is really cool!"; // Change the text here
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
